ad135e2bb0b742c2015b8bc14077d08a79aa9684
Md. Sharif Chowdhury <sharif.cse.hstu@gmail.com>
2021-03-11 00:33:19 +0600

    add day 06

---
+++ basic_01/day06/loop.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+//    cout << 1 << "\n";
+
+    int i;
+    for (i=1; i<=50; i=i+1){
+        cout << 2 * i << "\n";
+    }
+
+
+
+    return 0;
+}
